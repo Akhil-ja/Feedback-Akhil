f11c92742f1907141ecd020eb25e8a8885ae8329
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-05-12 12:37:16 +0530

    feat(backend,utils): add jwt utilities

---
+++ backend/src/utils/jwtUtils.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokens = (id: string, role: string) => {
+  const accessToken = jwt.sign({ id, role }, process.env.JWT_SECRET as string, {
+    expiresIn: '15m',
+  });
+
+  const refreshToken = jwt.sign(
+    { id, role },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    {
+      expiresIn: '7d',
+    }
+  );
+
+  return { accessToken, refreshToken };
+};
+
+export default generateTokens;
