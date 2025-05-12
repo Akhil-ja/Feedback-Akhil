79b76973f62d1ef5247a8ec6328942c9c944f196
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-05-12 14:20:53 +0530

    feat(frontend): add frontend folder with initial Vite setup.

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8000,
+    proxy: { "/api": { target: "http://localhost:3000" } },
+    changeOrigin: true,
+  },
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
